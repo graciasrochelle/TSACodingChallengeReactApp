a94a8e6a48ae94b75279cd97b39d211568fbcd19
rochelle <graciasrochelle@github.com>
2020-06-29 04:50:35 +1000

    Updates

--- precache-manifest.b16600368a12357fc576b2c3ae38db30.js
+++ precache-manifest.b16600368a12357fc576b2c3ae38db30.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b146662dd83af306c1d6e3aa1c170599",
+    "revision": "d88d90acee5d493288ba8a17f0665b2f",
     "url": "/TSACodingChallengeReactApp/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TSACodingChallengeReactApp/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "40c9e3e47f24a9ac5082",
+    "revision": "b7cbcfac51b5194d0755",
     "url": "/TSACodingChallengeReactApp/static/css/main.41a706b1.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TSACodingChallengeReactApp/static/js/2.361c9942.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "40c9e3e47f24a9ac5082",
-    "url": "/TSACodingChallengeReactApp/static/js/main.7001cca5.chunk.js"
+    "revision": "b7cbcfac51b5194d0755",
+    "url": "/TSACodingChallengeReactApp/static/js/main.64423f90.chunk.js"
   },
   {
     "revision": "fd07d34345dbc7fd7272",
