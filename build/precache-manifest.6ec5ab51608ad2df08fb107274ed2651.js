82598571f09aa3e0503b2dbc594570264e75b67e
rochelle <graciasrochelle@github.com>
2020-06-29 04:30:59 +1000

    build

---
+++ build/precache-manifest.6ec5ab51608ad2df08fb107274ed2651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ae21fd4f22b1734de3d7b1e3558516",
+    "url": "/TSACodingChallengeReactApp/index.html"
+  },
+  {
+    "revision": "db5e4e906149599f39b4",
+    "url": "/TSACodingChallengeReactApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7a77b032646fac4e0ac",
+    "url": "/TSACodingChallengeReactApp/static/css/main.41a706b1.chunk.css"
+  },
+  {
+    "revision": "db5e4e906149599f39b4",
+    "url": "/TSACodingChallengeReactApp/static/js/2.ef41cea5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TSACodingChallengeReactApp/static/js/2.ef41cea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a77b032646fac4e0ac",
+    "url": "/TSACodingChallengeReactApp/static/js/main.6cb8def3.chunk.js"
+  },
+  {
+    "revision": "fd07d34345dbc7fd7272",
+    "url": "/TSACodingChallengeReactApp/static/js/runtime-main.e4d4c2c1.js"
+  },
+  {
+    "revision": "466ec83dac3001b5d3b3f74ad97287b2",
+    "url": "/TSACodingChallengeReactApp/static/media/loading.466ec83d.gif"
+  }
+]);
